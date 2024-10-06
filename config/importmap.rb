7d4eddee02a5869feb22ebd4c0d7b00e35420ccd
Dirk Kramer <HydroToxin@users.noreply.github.com>
2024-10-06 23:00:40 +0200

    WIP: stable Rail 7 asset pipeline generation for production and development via esbuild

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,6 +1,1 @@
 # Pin npm packages by running ./bin/importmap
-
-pin "application", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
-pin "jquery", to: "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js", preload: true
