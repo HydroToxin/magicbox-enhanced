284c572f890b14603e74a4bb66fca984e439bdab
Dirk Kramer <HydroToxin@users.noreply.github.com>
2024-10-06 10:39:20 +0200

    change gitignore to filter out compiled asset files

---
+++ public/assets/observations-bc50c76850abbb62d4300e78ac37800a88eac4d5f2e42a4c3807ea8151b67fcc.js
@@ -0,0 +1,13 @@
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+
+// app/javascript/observations.js
+var require_observations = __commonJS({
+  "app/javascript/observations.js"(exports) {
+    (function() {
+    }).call(exports);
+  }
+});
+export default require_observations();
