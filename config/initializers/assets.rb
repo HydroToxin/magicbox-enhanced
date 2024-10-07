fc01b189d097b4850b127a5d7e7ad2241f5fad1c
Dirk Kramer <HydroToxin@users.noreply.github.com>
2024-10-07 19:49:21 +0200

    now we can remove old bootstrap and temporary build scripts. Add jsbundling-rails and cssbundling-rails as default build

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,11 +5,26 @@
 
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 #Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 #Rails.application.config.assets.paths = ["app/assets/builds","app/assets/config","app/assets/scss","app/assets/js"]
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
 
-Rails.application.config.assets.paths << Rails.root.join('public', 'fonts', 'font-awesome')
+Rails.application.config.assets.paths << Rails.root.join('public', 'fonts', 'font-awesome', 'node_modules')
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
+Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap/dist/js")
+Rails.application.config.assets.precompile << "bootstrap.min.js"
+Rails.application.config.assets.css_compressor = nil
+Rails.application.config.assets.paths << Rails.root.join("app/assets/builds")
