739d121fe0c4b217336cab271e1474def46cc994
Dirk Kramer <HydroToxin@users.noreply.github.com>
2024-12-01 17:35:16 +0100

    add builds to assets path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,6 +11,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap-icons/font')
 Rails.application.config.assets.paths << Rails.root.join('node_modules/bootstrap/dist/js')
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('app/assets/builds')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
