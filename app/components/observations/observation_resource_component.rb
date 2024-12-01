d6f34732e845195de9a1a3ca61a992d61a0c3c7c
Dirk Kramer <HydroToxin@users.noreply.github.com>
2024-12-01 19:29:25 +0100

    remove view components

--- app/components/observations/observation_resource_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class Observations::ObservationResourceComponent < ViewComponent::Base
-  def initialize(title:, content:)
-    @title = title
-    @content = content
-  end
-end
